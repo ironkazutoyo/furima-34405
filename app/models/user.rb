3ab4a1b3d2932b6469240fda8d26f46e186a7653
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-01 18:23:52 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   with_options presence: true do
     validates :nickname
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'} 
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'}
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters'}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters'}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' }
     validates :birthday
   end
-
 end
