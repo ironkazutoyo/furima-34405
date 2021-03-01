ace3d728d22fe756479068515155405ada4b517b
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-01 12:12:52 +0900

    バリデーションの設定　再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'} 
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birthday
+  end
 
 end
