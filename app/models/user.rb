aa130f1541f659671eb4093bf1f097f28b3ae1e0
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-01 17:57:46 +0900

    userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'} 
-    validates :first_name
-    validates :last_name
-    validates :first_name_kana
-    validates :last_name_kana
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters'}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters'}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters'}
     validates :birthday
   end
 
