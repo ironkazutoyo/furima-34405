d8e9361b545016d6b9a7a97246ee746f50712d9b
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-09 17:45:06 +0900

    電話番号のバリデーション修正

--- app/models/order_form_object.rb
+++ app/models/order_form_object.rb
@@ -3,11 +3,11 @@ class OrderFormObject
   attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :tel_num, :order_history_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, messages: 'Input correctly' }
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :municipality
     validates :address
-    validates :tel_num, format: {with: /[0-9]{,8}/}
+    validates :tel_num, format: {with: /\A\d{10,11}\z/, message: 'Input only number'}
     # validates :order_history_id
     validates :user_id
     validates :item_id
@@ -17,7 +17,6 @@ class OrderFormObject
     order_history = OrderHistory.create(user_id: user_id, item_id: item_id)
     Order.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, tel_num: tel_num, order_history_id: order_history.id )
   end
-
 
 
 
