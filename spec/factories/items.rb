25d55739cbcfec94fced82cf394e60da86d4a0d9
ironkazutoyo <78397291+ironkazutoyo@users.noreply.github.com>
2021-03-09 17:44:25 +0900

    他テストの記述簡易化の為、imageのfactorybot修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,9 +5,13 @@ FactoryBot.define do
     category_id              { Faker::Number.within(range: 2..11) }
     status_id { Faker::Number.within(range: 2..7) }
     shipping_charge_id { Faker::Number.within(range: 2..3) }
-    shipping_area_id { Faker::Number.within(range: 2..48) }
+    prefecture_id { Faker::Number.within(range: 2..48) }
     days_to_ship_id { Faker::Number.within(range: 2..4) }
     price       { Faker::Number.within(range: 300..9_999_999) }
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/flag.png'), filename: 'flag.png')
+    end
   end
 end
